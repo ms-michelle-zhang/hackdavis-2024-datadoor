be10cedc66c2e41d5405469bdfff82bd41bb7074
ms-michelle-zhang <michelleszhang@outlook.com>
2024-04-28 09:51:39 -0700

    Updated with Notepad++ code instead of Notepad code.

--- DataDoor/Geocode.py
+++ DataDoor/Geocode.py
@@ -1,47 +1,47 @@
 import geopy
 from geopy.geocoders import Nominatim
 
-def get_location(zip_code)
+def get_location(zip_code):
 
     ZIP = zip_code
-    geolocator = Nominatim(user_agent=DataDoor)
-    location = geolocator.geocode(United States {}.format(ZIP))
+    geolocator = Nominatim(user_agent="DataDoor")
+    location = geolocator.geocode("United States {}".format(ZIP))
     address = location.address
     print(address)
-    address_list = address.split(, )
+    address_list = address.split(", ")
 
     us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Washington DC', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
 
-    for subdivision in address_list
-        if subdivision in us_states
+    for subdivision in address_list:
+        if subdivision in us_states:
             state = subdivision
 
 
-    if len(address_list)5
-        if County in address_list[1]  
+    if len(address_list)<5:
+        if "County" in address_list[1]  :
             address_dict = {
-                ZIPaddress_list[0],
-                CityNone,
-                Countyaddress_list[1],
-                Statestate,
-                CountryUnited States
+                "ZIP":address_list[0],
+                "City":None,
+                "County":address_list[1],
+                "State":state,
+                "Country":"United States"
             }
-        else
+        else:
             address_dict = {       
-                ZIPaddress_list[0],
-                Cityaddress_list[1],
-                CountyNone,
-                Statestate,
-                CountryUnited States
+                "ZIP":address_list[0],
+                "City":address_list[1],
+                "County":None,
+                "State":state,
+                "Country":"United States"
             }
-    else
+    else:
         address_dict = {
-            ZIPaddress_list[0],
-            Cityaddress_list[1],
-            Countyaddress_list[2],
-            Statestate,
-            CountryUnited States
+            "ZIP":address_list[0],
+            "City":address_list[1],
+            "County":address_list[2],
+            "State":state,
+            "Country":"United States"
         }
 
     return(address_dict)
