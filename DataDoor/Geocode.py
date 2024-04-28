0a9e7cb988bf34f6bc5ee94e74b3707df1f808ee
ms-michelle-zhang <michelleszhang@outlook.com>
2024-04-28 07:48:04 -0700

    Added geocoding code.

---
+++ DataDoor/Geocode.py
@@ -0,0 +1,47 @@
+import geopy
+from geopy.geocoders import Nominatim
+
+def get_location(zip_code)
+
+    ZIP = zip_code
+    geolocator = Nominatim(user_agent=DataDoor)
+    location = geolocator.geocode(United States {}.format(ZIP))
+    address = location.address
+    print(address)
+    address_list = address.split(, )
+
+    us_states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Washington DC', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+
+    for subdivision in address_list
+        if subdivision in us_states
+            state = subdivision
+
+
+    if len(address_list)5
+        if County in address_list[1]  
+            address_dict = {
+                ZIPaddress_list[0],
+                CityNone,
+                Countyaddress_list[1],
+                Statestate,
+                CountryUnited States
+            }
+        else
+            address_dict = {       
+                ZIPaddress_list[0],
+                Cityaddress_list[1],
+                CountyNone,
+                Statestate,
+                CountryUnited States
+            }
+    else
+        address_dict = {
+            ZIPaddress_list[0],
+            Cityaddress_list[1],
+            Countyaddress_list[2],
+            Statestate,
+            CountryUnited States
+        }
+
+    return(address_dict)
